8f2fe931340fd6bb7f086657661cd7cc6d2692de
seongsiyeong <ssy2681q@naver.com>
2025-12-17 19:37:05 +0900

    fix(front): api 주소를 환경에 따라 다르게 설정하도록 수정

--- frontend/app/api/tmiApi.js
+++ frontend/app/api/tmiApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:8080/api";
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 export async function fetchTmis() {
   try {
